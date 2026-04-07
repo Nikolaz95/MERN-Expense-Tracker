246295cd695f628b423520a91904fea318dabff5
Nikola Zovko <nikolajoe95@gmail.com>
2026-04-07 17:44:43 +0200

    postingLocalServer

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,13 @@ import dotenv from "dotenv";
 dotenv.config({ path: "backend/config/config.env" });
 
 
+//import all routes
+import productRoutes from "./routes/products.js";
+
+app.use("/api", productRoutes)
+
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on Port: ${process.env.PORT}`);
+    console.log(`Server started on Port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 })
