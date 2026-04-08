f98b49f44f40737f0cb14d63c36dbd9552307a36
Nikola Zovko <nikolajoe95@gmail.com>
2026-04-09 00:48:09 +0200

    generate JsonToken

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -11,7 +11,9 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
         name, email, password,
     })
 
+    const token = user.getJwtToken();
+
     res.status(201).json({
-        success: true,
+        token,
     });
 });
