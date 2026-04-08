29ea3ccb5afeb2320e68dc0e54b83aa056ff9459
Nikola Zovko <nikolajoe95@gmail.com>
2026-04-08 16:04:23 +0200

    create validation & moongoseID error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,3 +1,4 @@
+import ErrorHandler from "../utils/errorHandler.js";
 
 
 
@@ -7,11 +8,29 @@ export default (err, req, res, next) => {
         message: err?.message || "Internal Server Error",
     };
 
+    //handle invalid mongoose ID error
+    if (err.name === "CastError") {
+        const message = `Resource not found. Invalid ${err?.path}`
+        error = new ErrorHandler(message, 404)
+    }
 
-    res.status(error.statusCode).json({
-        message: error.message,
-    });
+    //Handle Validation error
+    if (err.name === "ValidationError") {
+        const message = Object.values(err.errors).map((value) => value.message);
+        error = new ErrorHandler(message, 400)
+    }
 
+    // Handle Mongoose Duplicate Key Error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered.`;
+        error = new ErrorHandler(message, 400);
+    }
+
+    // Handle wrong JWT Error
+    if (err.name === "JsonWebTokenError") {
+        const message = `JSON Web Token is invalid. Try Again!!!`;
+        error = new ErrorHandler(message, 400);
+    }
 
     if (process.env.NODE_ENV === "DEVELOPMENT") {
         res.status(error.statusCode).json({
@@ -26,4 +45,7 @@ export default (err, req, res, next) => {
         });
     }
 
-}
+    res.status(error.statusCode).json({
+        message: error.message,
+    });
+};
