8d5ac672785fc18a0d1821ba8dfa815952d7b636
Nikola Zovko <nikolajoe95@gmail.com>
2026-04-11 01:34:44 +0200

    displayAllTransaction

--- backend/controllers/transactionControllers.js
+++ backend/controllers/transactionControllers.js
@@ -1,18 +1,18 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
-import newTransition from "../models/transaction.js";
+import Transition from "../models/transaction.js";
 
 // ➕ create new transaction  /transaction/add
 
 export const createNewTransaction = async (req, res, next) => {
     // Step 1: 
-    const transition = await newTransition.create(req.body);
+    const transition = await Transition.create(req.body);
     /* const { title, amount, type, category, description } = req.body;
     const userId = req.user.id;
     console.log("Adding to new transition:", { title, amount, type, category, description }); */
 
 
     // Step 2: create new Transition
-    /* const createTransition = await newTransition.create({
+    /* const createTransition = await Transition.create({
         user: userId,
         title, amount, type, category, description
     }); */
@@ -24,5 +24,19 @@ export const createNewTransaction = async (req, res, next) => {
         message: "Created new Transition successfully!",
         transition,
         /* createTransition, */
+    });
+}
+
+
+// Get user's transaction - /transaction/me
+export const getVisitiList = async (req, res, next) => {
+
+    const userTransactionList = await Transition.find() // Now using the query object
+        .sort({ date: -1 }); // Newest first
+
+    res.status(200).json({
+        success: true,
+        count: userTransactionList.length,
+        userTransactionList
     });
 }
--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createNewTransaction } from "../controllers/transactionControllers.js";
+import { createNewTransaction, getVisitiList } from "../controllers/transactionControllers.js";
 
 
 const router = express.Router();
 
 router.route("/transaction/add").post(createNewTransaction);
+router.route("/transaction/me").get(getVisitiList);
 
 
 
