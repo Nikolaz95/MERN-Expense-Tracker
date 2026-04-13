7a549e0c1fed920f07d501f4844d38141f1eae73
Nikola Zovko <nikolajoe95@gmail.com>
2026-04-13 15:40:29 +0200

    adminGetAllUserSpecUser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { getUserProfile, loginUser, logoutUser, registerUser, updatePassword, updateProfile } from "../controllers/authControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { getAllUsers, getUserDetails, getUserProfile, loginUser, logoutUser, registerUser, updatePassword, updateProfile } from "../controllers/authControllers.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 
 
 const router = express.Router();
@@ -15,7 +15,13 @@ router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 
+//admin
 
+router.route("/admin/allUsers").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+
+router
+    .route("/admin/users/:id")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
 
 
 export default router;
