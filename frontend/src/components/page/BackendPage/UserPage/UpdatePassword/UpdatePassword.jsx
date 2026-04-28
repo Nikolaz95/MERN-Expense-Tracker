86eeff561ea6d4d46aa4dacf1fed563355a4f065
Nikola Zovko <nikolajoe95@gmail.com>
2026-04-28 20:10:24 +0200

    updatePasswordFront

--- frontend/src/components/page/BackendPage/UserPage/UpdatePassword/UpdatePassword.jsx
+++ frontend/src/components/page/BackendPage/UserPage/UpdatePassword/UpdatePassword.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashBoardLayout from '../../Layouts/DashBoardLayout'
 import UserInfoLayout from '../LayoutUI/UserInfoLayout'
 import Button from '../../../../layouts/Buttons/Button'
 import titleName from '../../../../hooks/useTitle';
+import toast from 'react-hot-toast';
 
 //import css
 import "./UpdatePassword.css"
 import { iconUpdate } from '../../../../../assets/BtnIcons';
+import { useUpdatePasswordMutation } from '../../../../../redux/api/userApi';
+import { useNavigate } from 'react-router-dom';
 
 const UpdatePassword = () => {
     titleName('Update Password', iconUpdate);
-    const user = {
-        name: "nikola",
-        role: "admin",
-        email: "nikolajoe@gmail.com",
-        createdAt: "2026-04-15",
-        password: "123456"
-    }
+    const navigate = useNavigate();
+
+    const [oldPassword, setOldPassword] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [updatePassword, { isLoading, error, isSuccess }] =
+        useUpdatePasswordMutation();
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error?.data?.message);
+        }
+
+        if (isSuccess) {
+            toast.success("Password Updated");
+            navigate("/user/settings-Profile");
+        }
+    }, [error, isSuccess]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const userData = {
+            oldPassword,
+            password,
+        };
+
+        updatePassword(userData);
+    };
+
+
     return (
         <DashBoardLayout>
             <h1>Update Password</h1>
@@ -24,14 +51,14 @@ const UpdatePassword = () => {
 
                 <section className="updatePasswordSection">
                     <h1>Update Password:</h1>
-                    <form className="formUpdatePassword" /* onSubmit={submitHandler} */>
+                    <form className="formUpdatePassword" onSubmit={submitHandler}>
                         <label htmlFor="name_field" className="form-label">Old Password:</label>
                         <input
                             type="password"
                             id="name_field"
                             placeholder='user password'
-                            /* value={oldPassword} */
-                            /* onChange={(e) => setOldPassword(e.target.value)} */
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
                             className="form-control"
                             name="name" />
                         <label htmlFor="email_field" className="form-label">New Password:</label>
@@ -39,16 +66,14 @@ const UpdatePassword = () => {
                             type="password"
                             id="email_field"
                             placeholder='user new password'
-                            value={user.password}
-                            /* onChange={(e) => setPassword(e.target.value)} */
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="form-control"
                             name="email" />
                         <Button
                             icon={iconUpdate}
-                            variant="updatePassword" /* disabled={isLoading} */>
-                            {/* <Image variant="icon" src={SaveUpdate} className="iconBtns" /> */}
-                            Update Password
-                            {/* {isLoading ? "Updating..." : "Update Password"} */}
+                            variant="updatePassword" disabled={isLoading}>
+                            {isLoading ? "Updating..." : "Update Password"}
                         </Button>
                     </form>
                 </section>
