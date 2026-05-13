3b7fc23a3e4ed53b15cd86443d53af496dba8f68
Nikola Zovko <nikolajoe95@gmail.com>
2026-05-13 19:48:26 +0200

    updateDeploy

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,19 @@ import dotenv from "dotenv";
 import { connectDataBase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
-dotenv.config({ path: "backend/config/config.env" });
+// Load env variables
+let envPath = process.env.NODE_ENV === "PRODUCTION"
+    ? path.join(__dirname, ".env.production")
+    : path.join(__dirname, "config/config.env");
+
+dotenv.config({ path: envPath });
 
 //handle uncaught exceptions
 process.on("uncaughtException", (err) => {
@@ -16,8 +26,6 @@ process.on("uncaughtException", (err) => {
     console.log(`Shutting down  due to uncaught exceptions`);
     process.exit(1);
 })
-
-
 
 //connecting to database
 connectDataBase();
@@ -36,15 +44,26 @@ app.use("/api", authRoutes)
 app.use("/api", transactionRoutes)
 
 
+//connecting backend and frontend
+if (process.env.NODE_ENV === "PRODUCTION") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
+
 
 //using error middleware
 app.use(errorMiddleware);
 
 
+const PORT = process.env.PORT || 3000;
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on Port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
-})
+const server = app.listen(PORT, () => {
+    console.log(`Server start on PORT: ${PORT} in ${process.env.NODE_ENV} mode.`);
+});
 
 
 //handle unhandle promise promise rejection
