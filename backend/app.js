3118a7ae7015578235c54ad4eddb4657cf908342
Nikola Zovko <nikolajoe95@gmail.com>
2026-05-13 20:24:00 +0200

    updateDeployV3

--- backend/app.js
+++ backend/app.js
@@ -48,8 +48,10 @@ app.use("/api", transactionRoutes)
 if (process.env.NODE_ENV === "PRODUCTION") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    app.use((req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../frontend/dist/index.html")
+        );
     });
 }
 
